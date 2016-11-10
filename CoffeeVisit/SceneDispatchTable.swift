bfdfaced02510f1edd75f3b3173ca6ff9d9dda5a
Paul Stringer <paulstringer@mac.com>
2016-11-10 04:42:02 -0800

    Tidies Up Scene Dispatch Table

--- CoffeeVisit/SceneDispatchTable.swift
+++ CoffeeVisit/SceneDispatchTable.swift
@@ -1,4 +1,47 @@
-// Adds Conformance to Destination View Controllers That Require Preparation.
+//MARK: TODO: Investigate possibility to generate boiler plate from Storyboard file
+
+
+//Extends Visitor Protocol with Matching Operations for Handling Each VisitableScene
+
+extension StoryboardController {
+  
+  func visit(_ scene: ViewController) {
+    // Prepare ViewController
+  }
+  
+  func visit(_ scene: ChooseBeverageViewController) {
+    // Prepare ChooseBeverageViewController
+    scene.interactor = ChooseBeverageInteractor(view: scene)
+    scene.delegate = context
+  }
+  
+  func visit(_ scene: OrderSummaryViewController) {
+    // Prepare OrderSummaryViewController
+    scene.interactor = OrderSummaryInteractor(view: scene, context: context)
+  }
+  
+  func visit(_ scene: CustomiseOrderViewController) {
+    // Prepare CustomiseOrderViewController
+    scene.interactor = CustomiseOrderSummaryInteractor(view: scene, context: context)
+  }
+  
+  func visit(_ scene: OrderConfirmationViewController) {
+    // Prepare OrderConfirmationViewController
+    scene.navigationItem.hidesBackButton = true
+  }
+  
+  func visit(_ scene: ChooseSizeViewController) {
+    // Prepare ChooseSizeViewController
+    scene.interactor = ChooseSizeInteractor(view: scene)
+    scene.delegate = context
+  }
+  
+}
+
+
+//MARK: View Controller Visitor Opt-in
+
+//Adds Conformance to Destination View Controllers That Require Preparation.
 
 extension ViewController: StoryboardVisitableScene {
   func accept(visitor: StoryboardController) { visitor.visit(self) }
@@ -23,56 +66,3 @@ extension ChooseSizeViewController: StoryboardVisitableScene {
 extension CustomiseOrderViewController: StoryboardVisitableScene {
   func accept(visitor: StoryboardController) { visitor.visit(self) }
 }
-
-// Extends The Visitor Protocol with Operations for Handling Destinations
-
-extension StoryboardController {
-  
-  func visit(_ scene: ViewController) {
-    
-    // Prepare ViewController
-  
-  }
-  
-  func visit(_ scene: ChooseBeverageViewController) {
-    
-    // Prepare ChooseBeverageViewController
-    
-    scene.interactor = ChooseBeverageInteractor(view: scene)
-    scene.delegate = context
-  }
-  
-  func visit(_ scene: OrderSummaryViewController) {
-    
-    // Prepare OrderSummaryViewController
-
-    scene.interactor = OrderSummaryInteractor(view: scene, context: context)
-    
-  }
-  
-  func visit(_ scene: CustomiseOrderViewController) {
-    
-    // Prepare CustomiseOrderViewController
-    
-    scene.interactor = CustomiseOrderSummaryInteractor(view: scene, context: context)
-    
-    
-  }
-  func visit(_ scene: OrderConfirmationViewController) {
-    
-    // Prepare OrderConfirmationViewController
-    
-    scene.navigationItem.hidesBackButton = true
-  }
-  
-  func visit(_ scene: ChooseSizeViewController) {
-    
-    // Prepare ChooseSizeViewController
-    
-    scene.interactor = ChooseSizeInteractor(view: scene)
-    scene.delegate = context
-    
-  }
-  
-}
-
